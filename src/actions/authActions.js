2a3db705bdbe76b58cc23b2c19fc98c194e4dc89
Sherezz Grant <rezziemaven@gmail.com>
2018-08-03 20:38:08 +0200

    fix: added env variable to env file

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,8 +3,8 @@ import { CALL_API } from '../middleware/api'
 export const getUserInfo = () => ({
   type: 'GET_USER_INFO',
   [CALL_API]: {
-    // endpoint: `${process.env.REACT_APP_SERVER_URL}${process.env.REACT_APP_API_VERSION}/user/me`,
-    endpoint: `http://localhost:5000/v1/user/me`,
+    endpoint: `${process.env.REACT_APP_SERVER_URL}${process.env.REACT_APP_API_VERSION}/user/me`,
+    // endpoint: `http://localhost:5000/v1/user/me`,
 
   }
 })
@@ -12,9 +12,9 @@ export const getUserInfo = () => ({
 export const loginUser = (code) => ({
   type: 'LOGIN_USER',
   [CALL_API]: {
-    endpoint: `http://localhost:5000/v1/auth/callback${code}`
+    // endpoint: `http://localhost:5000/v1/auth/callback${code}`
 
-    // endpoint: `${process.env.}${process.env.REACT_APP_API_VERSION}/auth/callback${code}`
+    endpoint: `${process.env.REACT_APP_SERVER_URL}${process.env.REACT_APP_API_VERSION}/auth/callback${code}`
   }
 })
 
--- src/reducers/authenticationReducer.js
+++ src/reducers/authenticationReducer.js
@@ -7,36 +7,36 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-
+    // console.log(action.response,action.response.token);
     if (action.response && action.response.token) {
       return {
         ...state,
         token: action.response.token,
         isAuthenticated: true
       }
-    } 
+    }
 
     if (action.error === 'token expired') return initialState
-    
+
     switch (action.type) {
-      
+
       case 'GET_USER_INFO_SUCCESS':
       return {
         ...state,
         currentUser: action.response
       }
-      
+
       case 'GET_USER_INFO_REQUEST':
       case 'GET_USER_INFO_FAILURE':
 
-      
+
       case 'LOGIN_USER_REQUEST':
         console.log("USER LOGIN REQUESTED")
         return state
       case 'LOGIN_USER_FAILURE':
         console.log("USER_LOG_IN_FAILURE")
         return state
-        
+
       case 'LOGOUT_USER':
         return initialState
 
